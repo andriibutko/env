3c009a4acdf0d78ca72bacd943c7a4fec4fee63c
Andrii Butko <andriib@wix.com>
2024-02-09 10:00:40 +0100

    init

---
+++ flake.nix
@@ -0,0 +1,11 @@
+{
+  description = "A very basic flake";
+
+  outputs = { self, nixpkgs }: {
+
+    packages.x86_64-darwin.hello = nixpkgs.legacyPackages.x86_64-darwin.hello;
+
+    packages.x86_64-darwin.default = self.packages.x86_64-darwin.hello;
+
+  };
+}
