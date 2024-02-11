664d57224b95d07ef61246965f139c062fffc5b3
Andrii Butko <andriib@wix.com>
2024-02-11 13:04:03 +0100

    test

--- nix/home.nix
+++ nix/home.nix
@@ -58,20 +58,6 @@ in {
       lfs.enable = true;
     };
 
-    starship = {
-      enable = true;
-      # Configuration written to ~/.config/starship.toml
-      settings = {
-        # add_newline = false;
-
-        # character = {
-        #   success_symbol = "[➜](bold green)";
-        #   error_symbol = "[➜](bold red)";
-        # };
-
-        # package.disabled = true;
-      };
-    };
 
     # gpg = {
     #   enable = true;
